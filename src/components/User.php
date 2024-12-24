bea95a7f008bbd2fde846df1bc7bff0003a088d3
naodai <zhao_brain@163.com>
2024-12-24 17:04:33 +0800

    afterlogin 增加ai次数

--- src/components/User.php
+++ src/components/User.php
@@ -4,6 +4,7 @@ namespace ssqz\components;
 
 
 use ssqz\models\UserToken;
+use ssqz\services\Service;
 
 class User extends \yii\web\User
 {
@@ -13,8 +14,16 @@ class User extends \yii\web\User
 
         $authHeader = UserToken::getHeaderToken();
         if ($authHeader) {
+            /* @var $identity \ssqz\models\User */
             $identity = $this->identityClass;
             $this->setIdentity($identity::findIdentityByAccessToken($authHeader, false));
         }
+    }
+
+    public function afterLogin($identity, $cookieBased, $duration)
+    {
+        //智慧鼠ai优化次数添加
+        Service::api('POST', \Yii::getAlias('@jianliApi'), 'ai-optimize-internal/add', ['user_id' => $identity->getId()],'json');
+        parent::afterLogin($identity, $cookieBased, $duration);
     }
 }
